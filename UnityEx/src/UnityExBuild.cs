33e9ea28648cfd455d6fe37b9bb208eb5a22cd6d
Igor Karatayev <igor.cwer@gmail.com>
2018-04-09 12:33:27 +0300

    updates. working on automated building.

---
+++ UnityEx/src/UnityExBuild.cs
@@ -0,0 +1,36 @@
+using System;
+using System.IO;
+using Microsoft.Build.BuildEngine;
+using UnityEditor;
+using UnityEngine;
+
+
+
+namespace UnityEditorEx
+{
+    public class UnityExBuild
+    {
+        public static void Build(string solutionName)
+        {
+            UnityBuildLogger logger = new UnityBuildLogger();
+
+            try
+            {
+                Engine.GlobalEngine.BuildEnabled = true;
+                Engine.GlobalEngine.RegisterLogger(logger);
+                Engine.GlobalEngine.GlobalProperties.SetProperty("UNITYEDITORDATA_PATH", Path.Combine(EditorApplication.applicationPath, "Contents"));
+                Engine.GlobalEngine.GlobalProperties.SetProperty("Configuration", "Release Unity");
+
+                Project p = new Project(Engine.GlobalEngine);
+                p.BuildEnabled = true;
+                //p.SetProperty("")
+                p.Load(solutionName);
+                p.Build();
+            }
+            catch (Exception e)
+            {
+                Debug.LogError(e);
+            }
+        }
+    }
+}
