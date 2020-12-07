cc9e8ab4d46187cd499facdebfb7f7ff74a05ac7
Igor Karatayev <igor.cwer@gmail.com>
2020-12-08 00:43:16 +0300

    update

---
+++ Libraries/ProcGenEx.Test/Scripts/IcosahedronTest.cs
@@ -0,0 +1,46 @@
+﻿using ProcGenEx;
+using UnityDissolve;
+using UnityEngine;
+
+namespace ProcGenEx.Test
+{
+	public class IcosahedronTest : DissolvedMonoBehaviour
+	{
+		[Component]
+		public MeshFilter meshFilter;
+
+		public bool IsSimple = true;
+		public int Subdivisions = 0;
+		public int Steps = 1;
+
+#if UNITY_EDITOR
+		protected override void OnValidate()
+		{
+			base.OnValidate();
+	
+			var mb = IsSimple
+				? Icosahedron.CreateSimple()
+				: Icosahedron.Create();
+
+			for (int i = 0; i < Subdivisions; i++)
+			{
+				mb.Subdivide(Steps);
+			}
+
+			meshFilter.mesh = mb.ToMesh();
+		}
+#endif
+
+		// Start is called before the first frame update
+		void Start()
+		{
+
+		}
+
+		// Update is called once per frame
+		void Update()
+		{
+
+		}
+	}
+}
